e0cf0577ec4f61a60cb2b84c2b9169f02adbfa5d
pobmic15 <pobmic15@gasos-ro.cz>
2026-02-05 12:20:50 +0100

    e

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Uzivatel uzivatel1 = new Uzivatel("Pepa", "Doubravka", "pear3535_", "Kjaidd3835385385385");
+        /*Uzivatel uzivatel1 = new Uzivatel("Pepa", "Doubravka", "pear3535_", "Kjaidd3835385385385");
         System.out.println(uzivatel1.getJmeno());
         System.out.println(uzivatel1.getPrijmeni());
         System.out.println(uzivatel1.getUzivatelskeJmeno());
-        System.out.println(uzivatel1.getHeslo());
+        System.out.println(uzivatel1.getHeslo());*/
+
+        Uzivatel uzivatel1 = new Uzivatel("05.08.1987","18. 6. 2020","1871-07-11");
+        System.out.println(uzivatel1.getDatum1());
+        System.out.println(uzivatel1.getDatum2());
+        System.out.println(uzivatel1.getDatum3());
     }
 }
--- src/Uzivatel.java
+++ src/Uzivatel.java
@@ -1,5 +1,5 @@
 public class Uzivatel {
-    private String jmeno;
+    /*private String jmeno;
     private String prijmeni;
     private String uzivatelskeJmeno;
     private String heslo;
@@ -47,5 +47,43 @@ public class Uzivatel {
 
     public String getHeslo() {
         return heslo;
+    }*/
+
+    private String datum1;
+    private String datum2;
+    private String datum3;
+
+    private static final String regDatum1 = "^(([0-2][0-9]|3[0-1])){2}\\.[0-1]{2}\\.[0-9]{4}$";
+    private static final String regDatum2 = "^[0-9]{1,2}\\.\\s?[0-9]{1,2}\\.\\s?[0-9]{4}$";
+    private static final String regDatum3 = "^[0-9]{4}-[0-9]{2}-[0-9]{2}$";
+
+    public Uzivatel(String datum1, String datum2, String datum3) {
+        if (!datum1.matches(regDatum1)) {
+            throw new IllegalArgumentException("Neplatný datum1");
+        }
+
+        if (!datum2.matches(regDatum2)) {
+            throw new IllegalArgumentException("Neplatný datum2");
+        }
+
+        if (!datum3.matches(regDatum3)) {
+            throw new IllegalArgumentException("Neplatný datum3");
+        }
+
+        this.datum1 = datum1;
+        this.datum2 = datum2;
+        this.datum3 = datum3;
+    }
+
+    public String getDatum1() {
+        return datum1;
+    }
+
+    public String getDatum2() {
+        return datum2;
+    }
+
+    public String getDatum3() {
+        return datum3;
     }
 }
